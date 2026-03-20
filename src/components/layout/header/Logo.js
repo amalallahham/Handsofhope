c4d7547775a4e19a2bb31c7c2212b085c271c3be
Amal Allaham <amalallahham20@gmail.com>
2026-03-20 04:40:49 -0700

    fix logo

--- src/components/layout/header/Logo.js
+++ src/components/layout/header/Logo.js
@@ -3,24 +3,24 @@
 import Image from "next/image";
 import Link from "next/link";
 const Logo = ({ headerType, isStickyHeader }) => {
-	return (
-		<div className="site_logo">
-			<Link className="logo" href="/">
-				<Image
-					src={
-						
-					
-						 "/images/logos/logo-3.svg"
-							
-					}
-					alt=""
-					width={544}
-					height={152}
-					style={{ height: "auto", display: "block" }}
-				/>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="site_logo">
+      <Link className="logo" href="/">
+        <Image
+          src="/images/logos/logo-3.svg"
+          alt="Hands of Hope logo"
+          width={544}
+          height={152}
+          style={{
+            width: "100%",
+            maxWidth: "544px",
+            height: "auto",
+            display: "block",
+          }}
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default Logo;
